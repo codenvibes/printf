d7492de62daf77df29036356ab08e981c7ae38c4
codenvibes <terrymk1999@gmail.com>
2023-06-23 16:07:37 +0300

    Create header file

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * _printf - a function that produces output according to a format.
+ * @format: the format string.
+ *
+ * Return: Number of string characters.
+ */
+int _printf(const char *format, ...);
+
+#endif
