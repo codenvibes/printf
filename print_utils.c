c53383ae9b4c7100326d5a7e54a13ac90e0159fc
codenvibes <terrymk1999@gmail.com>
2023-06-26 17:50:56 +0300

    Handle the %S custom conversion specifier

--- print_utils.c
+++ print_utils.c
@@ -137,10 +137,28 @@ void print_octal(unsigned int num, int *count)
  */
 void print_non_printable(char c, int *count)
 {
+	char hex_digits[] = "0123456789ABCDEF";
+
 	print_char('\\', count);
 	print_char('x', count);
-
-	char hex_digits[] = "0123456789ABCDEF";
 	print_char(hex_digits[(c >> 4) & 0xF], count);
 	print_char(hex_digits[c & 0xF], count);
+}
+
+/**
+ * print_non_printable_string - Prints a string with non-printable characters in the format \x followed by the ASCII code value in hexadecimal.
+ * @str: String to be printed.
+ * @count: Pointer to the count of characters printed.
+ */
+void print_non_printable_string(const char *str, int *count)
+{
+	while (*str)
+	{
+		if (*str >= ' ' && *str < 127)
+			print_char(*str, count);
+		else
+			print_non_printable(*str, count);
+
+		str++;
+	}
 }
